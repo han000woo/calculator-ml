b86bcf42bdd4680453aa05ad44674e2d682ba1df
han000woo <gkstjsdn0011@naver.com>
2025-12-09 17:37:14 +0900

    ip허용

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # ⭐ CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React
+    allow_origins=["*"],  # React
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
